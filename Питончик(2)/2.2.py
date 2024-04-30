4c541c723448580663ca39e7b8c00762753f5074
Swarowskee <168467504+Swarowskee@users.noreply.github.com>
2024-04-30 20:38:38 +0300

    Add files via upload

---
+++ Питончик(2)/2.2.py
@@ -0,0 +1,13 @@
+﻿#coding=utf-8
+text = input("текст:")
+dct = {}
+
+for letter in text:
+    if letter != ' ':  
+        letter = letter.lower()  
+        if letter in dct:
+            dct[letter] += 1
+        else:
+            dct[letter] = 1
+
+print(dct)
