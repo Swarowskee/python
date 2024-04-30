032547dbd9bc962e5c470620695de8ab0d92fb21
Swarowskee <168467504+Swarowskee@users.noreply.github.com>
2024-04-30 20:43:09 +0300

    Add files via upload

---
+++ Питончик(2)/_2.1.py
@@ -0,0 +1,17 @@
+﻿#coding=utf-8
+числа = []
+экспонента = int(input("введите степень: "))
+количество_чисел = int(input("количество чисел в списке: "))
+
+for i in range(количество_чисел):
+    num = input("число, которое нужно возвести в степень: ")
+    числа.append(num)
+
+
+for num in числа:
+    if num.isdigit() or (num.startswith('-') and num[1:].isdigit()):  
+        num = float(num)  
+        powered_num = num ** экспонента
+        print(f"{num} в степени {экспонента} = {powered_num}")
+    else:
+        print(f"{num} * {экспонента} =", num * экспонента)  
