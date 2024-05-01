2b2ce52a523082f66b18e5a5d30f5fe846efe567
Swarowskee <168467504+Swarowskee@users.noreply.github.com>
2024-05-01 22:51:16 +0300

    Add files via upload

---
+++ Питончик(3)/_3.2.py
@@ -0,0 +1,9 @@
+﻿#coding=utf-8
+from typing import List
+def multiply_elements(elements: List[int], multiplier: int = 2) -> List[int]:
+    return [element * multiplier for element in elements]
+elements_input = input("введите числа через пробел: ")
+elements = [int(x) for x in elements_input.split()]
+multiplier_input = int(input("введите множитель (по базе 2): ") or "2")
+result = multiply_elements(elements, multiplier_input)
+print(result)
