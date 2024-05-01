4120cebb8d8109d8da13362975a5bf9c5108cb8e
Swarowskee <168467504+Swarowskee@users.noreply.github.com>
2024-05-01 22:37:08 +0300

    Add files via upload

---
+++ Питончик(3)/_3.2.2.py
@@ -0,0 +1,9 @@
+﻿#coding=utf-8
+multiply_lambda = lambda elements, multiplier=2: [element * multiplier for element in elements]
+elements_input = input("введите числа через пробел: ")
+elements = [int(x) for x in elements_input.split()]
+
+multiplier_input = int(input("введите множитель (базовый 2): ") or "2")
+
+result_lambda = multiply_lambda(elements, multiplier_input)
+print(result_lambda)
